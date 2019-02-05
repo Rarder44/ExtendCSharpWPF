13f0c8f45a2603e1ad307b7cedaa713b5ac1846b
Luca Pozzi <l.pozzi@outlook.com>
2019-02-05 17:34:44 +0100

    - Aggiunto NumericUpDown   - UserControl per la gestione di una casella di testo con input numerico e frecce laterali, con limiti min e max - Aggiunto PercentageConverter   - Converter per binding per impostare una percentuale al valore scelto

---
+++ Controls/NumericUpDown.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtendCSharpWPF.Controls
+{
+    /// <summary>
+    /// Logica di interazione per NumericUpDown.xaml
+    /// </summary>
+    public partial class NumericUpDown : UserControl
+    {
+        public int? Max { get; set; } = null;
+        public int? Min { get; set; } = null;
+        public int Default { get; set; } = 0;
+
+
+
+        public NumericUpDown()
+        {
+            InitializeComponent();
+            
+        }
+        private void userControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            _numValue = Default;
+            txtNum.Text = _numValue.ToString();
+        }
+        private int _numValue;
+        private bool DisableTextChanged = false;
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                DisableTextChanged = true;
+                txtNum.Text = value.ToString();
+                DisableTextChanged = false;
+            }
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            if( CheckNumber(NumValue+1))
+                NumValue++;
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckNumber(NumValue -1))
+                NumValue--;
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (DisableTextChanged)
+                return;
+            
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            int t;
+            if (int.TryParse(txtNum.Text, out t))
+            {
+                if (CheckNumber(t))
+                    NumValue = t;
+                else
+                    NumValue = NumValue;
+            }
+            else
+                NumValue = Default;
+        }
+
+
+        private bool CheckNumber(int n)
+        {
+            if (Max != null && n > Max)
+                return false;
+
+            if (Min != null && n < Min)
+                return false;
+
+            return true;
+               
+        }
+
+        
+    }
+}
